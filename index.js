2256454890906e30e34cdc77f5cb52af48e5a7cf
Andri Möll <andri@dot.ee>
2014-11-04 21:25:49 +0200

    Use MediumType.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var MediaType = require("media-type")
+var MediaType = require("medium-type")
 
 exports.parse = function(msg) {
   if (typeof msg["content-type"] != "string") return msg
 
   msg = Object.create(msg)
-  msg["content-type"] = MediaType.fromString(msg["content-type"])
+  msg["content-type"] = new MediaType(msg["content-type"])
   return msg
 }
--- test/index_test.js
+++ test/index_test.js
@@ -1,15 +1,12 @@
-var MEDIA_TYPE_PATH = "../node_modules/media-type"
-var MediaType = require(MEDIA_TYPE_PATH).fromString("").constructor
 var InternetMessage = require("internet-message")
 var Type = require("..")
 
 describe("InternetMessage.Type", function() {
   describe(".parse", function() {
-    it("must parse given Content-Type to an instance of MediaType", function() {
+    it("must parse given Content-Type to a media type object", function() {
       var msg = new InternetMessage({"Content-Type": "application/json"})
 
       var type = Type.parse(msg)["content-type"]
-      type.must.be.an.instanceof(MediaType)
       type.type.must.equal("application")
       type.subtype.must.equal("json")
     })
